19d32eff16bf66ab28aa05d777971bec4aaf6754
GoodLozer <rosskentrell12@gmail.com>
2024-09-06 13:57:36 -0400

    wrote template for numGuess

---
+++ 01_gaming_exercises/02_guess_a_number/guess_number.cs
@@ -0,0 +1,18 @@
+using System;
+clas guessNumber {
+  static void Main() {
+    int numGuess = 0;
+    int maxGuess = 3;
+
+    // Generate a secret number here
+    //while loop 
+      // allow the user to guess the number 
+      //Compare the guess to the secret number 
+      // If match print win, if > or < give a hint
+      //+1 to numGuess
+      //Check if numguess > maxGuess
+      // if true print a lose game message if false guess again 
+
+
+  }
+}
