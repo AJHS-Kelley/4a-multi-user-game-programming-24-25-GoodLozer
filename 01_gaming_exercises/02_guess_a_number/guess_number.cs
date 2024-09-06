fe9e1666e1147b7813993d0b09c44371676bb865
GoodLozer <rosskentrell12@gmail.com>
2024-09-06 14:23:26 -0400

    made the number guesser

--- 01_gaming_exercises/02_guess_a_number/guess_number.cs
+++ 01_gaming_exercises/02_guess_a_number/guess_number.cs
@@ -1,11 +1,23 @@
 using System;
-clas guessNumber {
+class guessNumber {
   static void Main() {
     int numGuess = 0;
     int maxGuess = 3;
+    int guess; 
 
     // Generate a secret number here
-    //while loop 
+    Random rnd = new Random(); // Create an object named 'rnd' that is a copy of the Random() class.
+    int secretNumber = rnd.Next(70); // generate from 0 to 69
+    //console.WriteLine(secretNumber) //Comment out after testing
+    //int secretNumber = rnd.Next(25,1000); generate from, 25 to 999
+
+    while(numGuess < maxGuess) 
+    {
+        Console.WriteLine("Please guess an integer between 0 and 69\n"); 
+        guess = Convert.ToInt32(console.ReadLine());
+        Console.WriteLine(guess);
+        numGuess++
+    }
       // allow the user to guess the number 
       //Compare the guess to the secret number 
       // If match print win, if > or < give a hint
