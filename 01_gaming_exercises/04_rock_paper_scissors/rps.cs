5dd4c4d3e2819664e36955ce9ad2c03b7a4be970
GoodLozer <rosskentrell12@gmail.com>
2024-09-16 13:55:02 -0400

    updated

--- 01_gaming_exercises/04_rock_paper_scissors/rps.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.cs
@@ -1,9 +1,42 @@
 using System;
-class guessNumber { 
-    string playerName = "testPlayer" 
+class rps {
+     static void Main() {
+
+      string playerName = "Test Player"; 
+      int playerScore = 0; 
+      int cpuScore = 0;
+      int playerChoice;
+      int cpuChoice;  
 
 
-    while playerscore 
+
+      
+
+      while (playerScore && cpuScore > 5)
+      {
+        Console.WriteLine ("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock. The winner scores a point. If each player picks the same no points are awarded. The first player to five points wins.\n");
+        
+        
+        if playerChoice = rock && cpuChoice = paper playerScore i++ 
+      } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+   
+   
     
 } 
   
