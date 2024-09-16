3e04d49d29735b15485e3cd3099e81bd6932657f
GoodLozer <rosskentrell12@gmail.com>
2024-09-16 14:33:27 -0400

    attempted to make the while loop

--- 01_gaming_exercises/04_rock_paper_scissors/rps.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.cs
@@ -3,22 +3,61 @@ class rps {
      static void Main() {
 
       string playerName = "Test Player"; 
-      int playerScore = 0; 
-      int cpuScore = 0;
+      int playerScore; 
+      int cpuScore ;
       int playerChoice;
       int cpuChoice;  
+      string rock = "rock"; 
+      string paper = "paper";
+      string scissors = "scissors";
+      string rps; 
 
 
 
       
 
-      while (playerScore && cpuScore > 5)
-      {
-        Console.WriteLine ("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock. The winner scores a point. If each player picks the same no points are awarded. The first player to five points wins.\n");
+      while(playerScore || cpuScore != 5) 
+    {
+        Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock. The winner scores a point. If each player picks the same no points are awarded.The first player to five points wins\n"); 
+    
+        if (playerChoice = "rock" && cpuChoice = "scissors")
+        {
+            playerScore + 1; 
+            Console.WriteLine("Score is\n" playerScore +, cpuScore);
+        }
+        else if (playerChoice = "scissors" && cpuChoice = "paper")
+        {
+            playerScore + 1;
+            Console.WriteLine("Score is\n" playerScore +, cpuScore);
+        }
+
+        {
+        else if (playerChoice = "paper" && cpuChoice = "rock")
         
+            playerScore + 1;
+            Console.WriteLine("Score is\n" playerScore +, cpuScore);
+        }
+        if (cpuChoice = "rock" && playerChoice = "scissors")
+        {
+            playerScore + 1; 
+            Console.WriteLine("Score is\n" playerScore +, cpuScore);
+        }
+        else if (cpuChoice = "scissors" && playerChoice = "paper")
+        {
+            cpuScore + 1;
+            Console.WriteLine("Score is\n" playerScore +, cpuScore);
+        }
+
+        {
+        else if (cpuChoice = "paper" && playerChoice = "rock")
         
-        if playerChoice = rock && cpuChoice = paper playerScore i++ 
-      } 
+            cpuScore + 1;
+            Console.WriteLine("Score is\n" playerScore +, cpuScore);
+        }
+
+
+        
+    }
 
 
 
